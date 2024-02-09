a8e9905da3d1830d00657b186aaf6895c7e8336f
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2024-02-10 00:05:25 +0100

    Added key + key in invetory

--- js/main.js
+++ js/main.js
@@ -4,12 +4,16 @@ window.onload = (event) => {
     //game window reff
     const gameWindow = document.getElementById("gameWindow");
 
-
+    //inventory
+    const inventory = document.getElementById("inventoryBox");
+    let keyItem = document.getElementById("keyItem")
     //main character 
     const mainCharacter = document.getElementById("mainCharacter");
     const offsetCharacter = 16;
 
     const tree1 = document.getElementById("squareTree");
+    const door = document.getElementById("door");
+    const key = document.getElementById("key");
     gameWindow.onclick = function (e) {
         let rect = gameWindow.getBoundingClientRect();
         let y = e.clientY - rect.top;
@@ -19,16 +23,23 @@ window.onload = (event) => {
         mainCharacter.style.top = `${y - 16}px`;
         mainCharacter.style.left = `${x - 16}px`;
 
-        console.log("y is " + y + "x is " + x)
 
-        console.log(e.target.id)
-
+        setObjectsOpacity();
         switch (e.target.id) {
             case "squareTree":
                 tree1.style.opacity = 0.5;
                 break;
+            case "door":
+                door.style.opacity = 0.5;
+                break;
+            case "key":
+                key.style.opacity = 0.5;
+                test();
+
+
+                break;
             default:
-                tree1.style.opacity = 1;
+
                 break;
         }
 
@@ -36,6 +47,16 @@ window.onload = (event) => {
 
     //fix: character going out of bounds
 
-
-
+    const setObjectsOpacity = () => {
+        tree1.style.opacity = 1;
+        door.style.opacity = 1;
+        key.style.opacity = 1;
+    }
+    async function test() {
+        console.log('start timer');
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        console.log('after 1 second');
+        document.getElementById("key").remove();
+        keyItem.innerText = "-Key"
+    }
 }
