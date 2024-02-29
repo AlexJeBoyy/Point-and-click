c3ba6d64ae3febf69149cfdf3a37008b4f49c79b
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2024-02-29 12:10:02 +0100

    added world2

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ window.onload = (event) => {
 
     //game window reff
     const gameWindow = document.getElementById("gameWindow");
-
+    const sec = 1000;
     //inventory
     let inventory = [];
     console.log(inventory)
@@ -18,11 +18,18 @@ window.onload = (event) => {
     const mainSpeach = document.getElementById("mainSpeach");
     const counterSpeach = document.getElementById("counterSpeach");
     // audio for dialog
-    const heroAudio = document.getElementById("heroAudio");
+    const mainAudio = document.getElementById("mainAudio");
     const counterAudio = document.getElementById("counterAudio");
 
     //avatar
     const counterAvatar = document.getElementById("counterAvatar");
+
+    //worlds
+    const world1 = document.getElementById("world1");
+    const world2 = document.getElementById("world2");
+    //bools
+    let doorUnlocked = false;
+
 
     gameWindow.onclick = function (e) {
         let rect = gameWindow.getBoundingClientRect();
@@ -39,19 +46,30 @@ window.onload = (event) => {
             //setObjectsOpacity();
 
             switch (e.target.id) {
-                case "squareTree":
-
+                case "statue":
+                    showMessage(mainSpeach, "Hey a statue.. Looks okay.", mainAudio);
+                    setTimeout(function () { counterAvatar.style.opacity = 1; }, 4 * sec);
+                    setTimeout(showMessage, 4.1 * sec, counterSpeach, "I can talk you know..", counterAudio);
+                    setTimeout(showMessage, 8.1 * sec, mainSpeach, "Wait what? That's not normal", mainAudio);
+                    setTimeout(showMessage, 12.1 * sec, counterSpeach, "Just shut up.. You want a key.. Check the pillar.", counterAudio);
+                    setTimeout(function () { counterAvatar.style.opacity = 0; }, 16 * sec);
                     break;
                 case "doorWizardTower":
-                    if (checkItem("Rusty Key")) {
-                        showMessage(mainSpeach, "I opened the door. Yeah!", heroAudio)
-                        console.log("I opened the door. Yeah!");
+                    if (doorUnlocked) {
+                        world1.style.display = 'none';
+                        world2.style.display = 'block';
+                    }
+                    else if (checkItem("Rusty Key")) {
+                        showMessage(mainSpeach, "I opened the door. Yeah!", mainAudio)
+                        setTimeout(function () { counterAvatar.style.opacity = 1; }, 4 * sec);
+                        setTimeout(showMessage, 4.1 * sec, counterSpeach, "Click on the door a second time to enter :)", counterAudio);
+                        setTimeout(function () { counterAvatar.style.opacity = 0; }, 16 * sec);
+                        doorUnlocked = true;
                     } else if (checkItem("Coin")) {
                         removeItem("Coin", "coin")
                         showMessage(mainSpeach, "Oh no I lost the coin and it didn't open the door.. Feel kinda stupid..", heroAudio);
-                        console.log("Oh no I lost the coin and it didn't open the door.. Feel kinda stupid..");
                     } else {
-                        console.log("Fuck this door is locked and I don't have a key. boohoo :(");
+                        showMessage(mainSpeach, "Fuck this door is locked and I don't have a key. boohoo :(", heroAudio);
                     }
                     break;
                 case "key":
@@ -61,6 +79,7 @@ window.onload = (event) => {
                 case "pond":
                     getItem("Coin", "coin");
                     break;
+
                 default:
 
                     break;
