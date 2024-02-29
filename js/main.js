a79015f311a10c7f8d5b022099e6deaf1e4042e9
AlexJeBoyy <a.strikwerda@sintlucasedu.nl>
2024-02-29 10:04:45 +0100

    added sound
    movement needs to be fixed

--- js/main.js
+++ js/main.js
@@ -9,53 +9,72 @@ window.onload = (event) => {
     console.log(inventory)
     const inventoryBox = document.getElementById("inventoryBox");
     const inventoryList = document.getElementById("inventoryList");
+
     //main character 
     const mainCharacter = document.getElementById("mainCharacter");
     const offsetCharacter = 16;
 
-    //const tree1 = document.getElementById("squareTree");
-    //const door = document.getElementById("door");
-    //const key = document.getElementById("key");
+    //speach bubbles
+    const mainSpeach = document.getElementById("mainSpeach");
+    const counterSpeach = document.getElementById("counterSpeach");
+    // audio for dialog
+    const heroAudio = document.getElementById("heroAudio");
+    const counterAudio = document.getElementById("counterAudio");
+
+    //avatar
+    const counterAvatar = document.getElementById("counterAvatar");
+
     gameWindow.onclick = function (e) {
         let rect = gameWindow.getBoundingClientRect();
         let y = e.clientY - rect.top;
         let x = e.clientX - rect.left;
 
-        //move character
-        if (e.target.id !== "mcImage") {
-            mainCharacter.style.top = `${y - offsetCharacter}px`;
-            mainCharacter.style.left = `${x - offsetCharacter}px`;
+        if (counterSpeach.style.opacity == 0 && mainSpeach.style.opacity == 0) {
+            if (e.target.id !== "mcImage") {
+                mainCharacter.style.top = `${y - offsetCharacter}px`;
+                mainCharacter.style.left = `${x - offsetCharacter}px`;
+            }
+
+
+            //setObjectsOpacity();
+
+            switch (e.target.id) {
+                case "squareTree":
+
+                    break;
+                case "doorWizardTower":
+                    if (checkItem("Rusty Key")) {
+                        showMessage(mainSpeach, "I opened the door. Yeah!", heroAudio)
+                        console.log("I opened the door. Yeah!");
+                    } else if (checkItem("Coin")) {
+                        removeItem("Coin", "coin")
+                        showMessage(mainSpeach, "Oh no I lost the coin and it didn't open the door.. Feel kinda stupid..", heroAudio);
+                        console.log("Oh no I lost the coin and it didn't open the door.. Feel kinda stupid..");
+                    } else {
+                        console.log("Fuck this door is locked and I don't have a key. boohoo :(");
+                    }
+                    break;
+                case "key":
+                    getItem("Rusty Key", "rustyKey");
+                    //ChangeInventory('key', "add")
+                    break;
+                case "pond":
+                    getItem("Coin", "coin");
+                    break;
+                default:
+
+                    break;
+            }
         }
-
-        //setObjectsOpacity();
-
-        switch (e.target.id) {
-            case "squareTree":
-
-                break;
-            case "doorWizardTower":
-                if (checkItem("Rusty Key")) {
-                    console.log("I opened the door. Yeah!");
-                } else if (checkItem("Coin")) {
-                    removeItem("Coin", "coin")
-                    console.log("Oh no I lost the coin and it didn't open the door.. Feel kinda stupid..");
-                } else {
-                    console.log("Fuck this door is locked and I don't have a key. boohoo :(");
-                }
-                break;
-            case "key":
-                getItem("Rusty Key", "rustyKey");
-
-                break;
-            case "pond":
-                getItem("Coin", "coin");
-                break;
-            default:
-
-                break;
-        }
-
     }
+    /**
+     * Adds or removes item from inv
+     * @param {*} itemName 
+     * @param {*} ation 
+     */
+
+
+
     /**
   * Checks if the value exists within the array
   * If not then it adds value to the array and use showItem function
@@ -107,14 +126,39 @@ window.onload = (event) => {
         document.getElementById(itemId).remove();
     }
 
-    //const setObjectsOpacity = () => {
-    //  tree1.style.opacity = 1;
-    // door.style.opacity = 1;
-    // key.style.opacity = 1;
-    //}
+
     async function test() {
         await new Promise(resolve => setTimeout(resolve, 1000));
         document.getElementById("key").remove();
         keyItem.innerText = "-Key"
     }
+
+    /**
+     * it will show dialoge.
+     * @param {getElementById} targetBubble 
+     * @param {string} message 
+     * @param {getElementById} targetSound 
+     */
+
+
+    function showMessage(targetBubble, message, targetSound) {
+        targetSound.currentTime = 0;
+        targetSound.play();
+        targetBubble.innerText = message;
+        targetBubble.style.opacity = 1;
+        setTimeout(hideMessage, 4 * sec, targetBubble, targetSound);
+    }
+    /**
+         * Hides message and pauze the audio
+         * @param {getElementById} targetBubble 
+         * @param {getElementById} targetSound 
+         */
+    function hideMessage(targetBubble, targetSound) {
+        targetSound.pause();
+        targetBubble.innerText = "...";
+        targetBubble.style.opacity = 0;
+    }
+
 }
+
+//runGame();
